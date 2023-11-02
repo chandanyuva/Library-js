cb160dbce45edbd493ac0fc5e32b93264bc25126
Chandan K P <chandankp.xworkz@gmail.com>
2023-11-02 16:05:56 +0530

    Compleated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const addBookDialogeBtn = document.getElementById("addBook-Dialog");
+const addBookDialogeBtn = document.getElementById("addBookBtn");
 const addBookDialoge=document.getElementById("adding-book");
 const input=addBookDialoge.querySelectorAll("form > input");
 const confirmBtn=addBookDialoge.querySelector("#confirmBtn");
@@ -13,21 +13,25 @@ confirmBtn.addEventListener("click",(event)=>{
     addBookDialoge.close(addBookToLibrary(input));
 });
 
-const myLibrary=[];
+const myLibrary=[{ title: "The Lord of the Rings", author: "Tolkien",noPages:0,  read_status: true },
+                {title: "Alice in Wonderland",author: "Lewis Caroll",noPages:0, read_status: false},
+                { title: "Naruto", author: "Masashi Kishimoto",noPages:0,  read_status: true },];
+showBooks();
 
-function Book(author,title,noPages,read){
+function Book(author,title,noPages,read_status){
     this.author=author;
     this.title=title;
     this.noPages=noPages;
-    this.read=read;
+    this.read_status=read_status;
 }
 
 function addBookToLibrary(nodeList){
     author=nodeList[0].value;
     title=nodeList[1].value;
     noPages=nodeList[2].value;
-    read=nodeList[3].value;
-    let book=new Book(author,title,noPages,read);
+    console.log(nodeList[3]);
+    read_status=nodeList[3].checked;
+    let book=new Book(author,title,noPages,read_status);
     myLibrary.push(book)
     showBooks();
 }
@@ -39,14 +43,14 @@ function createRow(){
     author=Row.insertCell(1);
     title=Row.insertCell(2);
     pages=Row.insertCell(3);
-    read=Row.insertCell(4);
+    read_status=Row.insertCell(4);
     removeBtn=Row.insertCell(5);
 
     return Row;
 }
 
 function removeBook(index){
-    console.log(index);
+
     table.deleteRow(index-1);
     myLibrary.splice(index-1,1);
     showBooks()
@@ -56,14 +60,17 @@ function showBooks(){
     table.innerHTML="";
     myLibrary.forEach((book)=>{
         curentRow=createRow();
-        curentRow.cells[0].innerText=myLibrary.indexOf(book)+1;
+        const index=myLibrary.indexOf(book)+1
+        curentRow.cells[0].innerText=index;
         curentRow.cells[1].innerText=book.author;
         curentRow.cells[2].innerText=book.title;
         curentRow.cells[3].innerText=book.noPages;
-        console.log(book.read);
-        curentRow.cells[4].innerText=book.read==="checked"?"yes":"Not Yet";
-        curentRow.cells[5].innerHTML=`<button onclick="removeBook(${curentRow.cells[0].innerText})">Delete</button>`;
+        curentRow.cells[4].innerHTML=`<input type="checkbox" ${book.read_status?"checked":''} id="${index}"></input>`;
+        const checkbox=document.getElementById(`${index}`);
+        checkbox.addEventListener('click',(e)=>{
+            myLibrary[index-1].read_status=checkbox.checked
+            console.log(myLibrary[index-1].read_status);
+        })
+        curentRow.cells[5].innerHTML=`<button onclick="removeBook(${curentRow.cells[0].innerText})" class="delBtn">Delete</button>`;
     })
 }
-
-// check box not working
